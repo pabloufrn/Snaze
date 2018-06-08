96990b778de23226a1d33a9c7bb7cdbe8098a97f
Pablo Emanuell Lopes Targino <pabloemanuell@academico.ufrn.br>
2018-06-07 23:22:15 -0300

    codigo a melhorar

---
+++ include/pause_game_state.hpp
@@ -0,0 +1,39 @@
+#ifndef PAUSE_GAME_STATE_H
+#define PAUSE_GAME_STATE_H
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+#include "state.hpp"
+
+#include "screen.hpp"
+
+#include "main_game_state.hpp"
+
+class MainMenuState : public State
+{
+    public:
+        
+        MainMenuState(sf::RenderTarget& target, StateMachine* state_machine);
+        
+        void init();
+        void process_events(sf::Event&);
+        void update();
+        void render();
+        
+        // --- Callbacks ---
+        void start_game();
+        void start_game_ai();
+        void exit_game();
+//         void about_game();
+//         void help_game();
+        
+    private:
+        sui::Screen m_menu_screen;
+        StateMachine* m_machine;
+        sf::RenderTarget* m_window;
+};
+
+
+
+#endif
---
+++ src/pause_game_state.cpp
@@ -0,0 +1,26 @@
+// #include "pause_game_state.hpp"
+// 
+// class PauseGame : public State
+// {
+//     public:
+//         
+//         PauseGame(sf::RenderTarget& target, StateMachine* state_machine, State game_state);
+//         
+//         void init();
+//         void process_events(sf::Event&);
+//         void update();
+//         void render();
+//         
+//         // --- Callbacks ---
+//         void resume_game();
+//         
+//     private:
+// 
+//         sui::Screen m_pause_screen;
+//         StateMachine* m_machine;
+//         sf::RenderTarget* m_window;
+// };
+// 
+// 
+// 
+// #endif
